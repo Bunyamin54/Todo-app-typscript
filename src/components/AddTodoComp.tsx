83b38220245b4e7388db32be57bb5df1197ab0c0
Bunyamin Genc <bunyaming@uia.no>
2023-08-28 20:56:43 +0200

    todo App with Typescript

--- src/components/AddTodoComp.tsx
+++ src/components/AddTodoComp.tsx
@@ -1,8 +1,21 @@
-import { Box, Button, Container, TextField } from '@mui/material'
+import { Box, Button, Container, TextField } from "@mui/material";
 import SaveIcon from "@mui/icons-material/Save";
-import React from 'react'
+import React, { useState } from "react";
 
-const AddTodoComp = () => {
+interface IAddTodoComp {
+  // addTodo: (text: string) => Promise<void>;
+  addTodo: AddFn;
+}
+
+const AddTodoComp = ({ addTodo }: {addTodo:(text: string) => Promise<void>}) => {
+  // const [text,setText] = useState<string>("")
+  const [text, setText] = useState(""); //! her zaman type belirtmemize gerek yok. Typescript type inference özelliği sayesinde inital değerine göre otomatik type ataması yapıyor.
+
+  const handleClick = () => {
+    console.log(text);
+    setText("");
+  };
+
   return (
     <Container>
       <Box
@@ -12,13 +25,26 @@ const AddTodoComp = () => {
           m: { xs: 4, sm: "auto" },
           height: { xs: "120px", sm: "80px" },
         }}>
-        <TextField id="outlined-basic" label="New Todo" variant="outlined" />
-        <Button variant="contained" endIcon={<SaveIcon />}>
-          Send
+        <TextField
+          id="outlined-basic"
+          label="New Todo"
+          sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
+          variant="outlined"
+          value={text}
+          onChange={e => setText(e.target.value)}
+          inputProps={{ maxLength: 40 }}
+        />
+        <Button
+          variant="contained"
+          onClick={handleClick}
+          disabled={!text.trim()}
+          sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
+          endIcon={<SaveIcon />}>
+          Save Todo
         </Button>
       </Box>
     </Container>
   );
-}
+};
 
-export default AddTodoComp
+export default AddTodoComp;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,64 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-
- interface TodoType  {
- todo : string
- isDone : boolean
- id: string | number //* id bilgisi string yada number olabilir
-
- }
-const Home = () => {
-
-const [todos, setTodos] = useState<TodoType[]>([])
-   
-  const url:string = import.meta.env.VITE_BASE_URL
-  const getTodos = async () => 
-   try {
-    
-    const {data} = await axios<TodoType[]>(url)
-
-   } catch (error) {
-    
-   }
-
-
-  return <div onClick={()=> setTodos([{id:5, todo:"eren", isDone:"true"}])}>Home</div>
-  
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Typography } from "@mui/material";
+import AddTodoComp from "../components/AddTodoComp";
+interface TodoType {
+  todo: string;
+  isDone: boolean;
+  id: string | number; // id bilgisi string yada number olabilir. İki veri tipinide kabul edecek.
 }
 
-export default Home
+const Home = () => {
+  // const [todos, setTodos] = useState([] as TodoType[]);
+  // const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const url: string = import.meta.env.VITE_BASE_URL;
+
+  const getTodos = async () => {
+    try {
+      // const {data} = await axios(url)
+      const { data } = await axios<TodoType[]>(url);
+      console.log(data);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // const addTodo = async (text:string) => {
+  //   try {
+
+  //   } catch (error) {}
+  // };
+  type AddFn = (text: string) => Promise<void>;
+
+  const addTodo: AddFn = async text => {
+    try {
+      await axios.post(url, { todo: text, isDone: false });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      getTodos();
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <Container>
+      <Typography
+        color="error"
+        align="center"
+        variant="h2"
+        component={"h1"}
+        mt={3}>
+        Todo App with Typescript
+      </Typography>
+      <AddTodoComp addTodo={addTodo} />
+    </Container>
+  );
+};
+
+export default Home;
