cf58baff8c5bfff17767be2ca085bff4c7373bfc
Bunyamin Genc <bunyaming@uia.no>
2023-08-28 19:44:21 +0200

    todo app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+ interface TodoType  {
+ todo : string
+ isDone : boolean
+ id: string | number //* id bilgisi string yada number olabilir
+
+ }
 const Home = () => {
+
+const [todos, setTodos] = useState<TodoType[]> ([])
+
+
+
   return (
     <div>Home</div>
   )
